43c65bc65b0c7922caa4d8184e407ddf9d81e22d
Максим Худяев <mr.yayay01@bk.ru>
2021-03-27 12:49:42 +0300

    Minor

--- Web/obj/Debug/net5.0/RazorDeclaration/Pages/PlusModal.razor.g.cs
+++ Web/obj/Debug/net5.0/RazorDeclaration/Pages/PlusModal.razor.g.cs
@@ -13,91 +13,91 @@ namespace Test.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "d:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
+#line 1 "C:\pepos\complex-task-apps\Web\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "d:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
+#line 2 "C:\pepos\complex-task-apps\Web\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "d:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
+#line 3 "C:\pepos\complex-task-apps\Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "d:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
+#line 4 "C:\pepos\complex-task-apps\Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "d:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
+#line 5 "C:\pepos\complex-task-apps\Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "d:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
+#line 6 "C:\pepos\complex-task-apps\Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "d:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
+#line 7 "C:\pepos\complex-task-apps\Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "d:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
+#line 8 "C:\pepos\complex-task-apps\Web\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "d:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
+#line 9 "C:\pepos\complex-task-apps\Web\_Imports.razor"
 using Test;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "d:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
+#line 10 "C:\pepos\complex-task-apps\Web\_Imports.razor"
 using Test.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "d:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
+#line 11 "C:\pepos\complex-task-apps\Web\_Imports.razor"
 using BlazorStrap;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "d:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\Pages\PlusModal.razor"
+#line 4 "C:\pepos\complex-task-apps\Web\Pages\PlusModal.razor"
 using Test.Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "d:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\Pages\PlusModal.razor"
+#line 5 "C:\pepos\complex-task-apps\Web\Pages\PlusModal.razor"
 using Test.Data.Modal;
 
 #line default
@@ -111,7 +111,7 @@ using Test.Data.Modal;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 56 "d:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\Pages\PlusModal.razor"
+#line 56 "C:\pepos\complex-task-apps\Web\Pages\PlusModal.razor"
       
     ////////////////////////////////////////////////////////////////
     private BSModal Modal { get; set; }
