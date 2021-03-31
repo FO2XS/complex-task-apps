e0dd63a70d179fa79a5ad352539641ed9fe5c296
FO2XS <foxxsf@gmail.com>
2021-03-31 18:34:17 +0300

    minor

--- Web/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ Web/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,84 +13,84 @@ namespace Test.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\pepos\complex-task-apps\Web\_Imports.razor"
+#line 1 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\pepos\complex-task-apps\Web\_Imports.razor"
+#line 2 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\pepos\complex-task-apps\Web\_Imports.razor"
+#line 3 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\pepos\complex-task-apps\Web\_Imports.razor"
+#line 4 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\pepos\complex-task-apps\Web\_Imports.razor"
+#line 5 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\pepos\complex-task-apps\Web\_Imports.razor"
+#line 6 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\pepos\complex-task-apps\Web\_Imports.razor"
+#line 7 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\pepos\complex-task-apps\Web\_Imports.razor"
+#line 8 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\pepos\complex-task-apps\Web\_Imports.razor"
+#line 9 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
 using Test;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\pepos\complex-task-apps\Web\_Imports.razor"
+#line 10 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
 using Test.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 11 "C:\pepos\complex-task-apps\Web\_Imports.razor"
+#line 11 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
 using BlazorStrap;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\pepos\complex-task-apps\Web\Shared\MainLayout.razor"
+#line 3 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\Shared\MainLayout.razor"
 using Test.Pages;
 
 #line default
@@ -104,26 +104,26 @@ using Test.Pages;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 49 "C:\pepos\complex-task-apps\Web\Shared\MainLayout.razor"
+#line 14 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\Shared\MainLayout.razor"
       
 
-    private Boolean isServerSide = false;
-    protected override async System.Threading.Tasks.Task OnInitializedAsync()
-    {
-        try
-        {
-            await BootstrapCSS.SetBootstrapCss("4.3.1");
-        }
-        catch (Exception)
-        {
-            isServerSide = true;
-        }
-    }
-    protected override async System.Threading.Tasks.Task OnAfterRenderAsync(Boolean firstRun)
-    {
-        if (isServerSide && firstRun)
-            await BootstrapCSS.SetBootstrapCss();
-    }
+	private Boolean isServerSide = false;
+	protected override async System.Threading.Tasks.Task OnInitializedAsync()
+	{
+		try
+		{
+			await BootstrapCSS.SetBootstrapCss("4.3.1");
+		}
+		catch (Exception)
+		{
+			isServerSide = true;
+		}
+	}
+	protected override async System.Threading.Tasks.Task OnAfterRenderAsync(Boolean firstRun)
+	{
+		if (isServerSide && firstRun)
+			await BootstrapCSS.SetBootstrapCss();
+	}
 
 #line default
 #line hidden
