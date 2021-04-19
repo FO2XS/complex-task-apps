c2ced5c828b0efdd75c195ce46f552946252995f
FO2XS <foxxsf@gmail.com>
2021-04-20 01:32:46 +0300

    minor

--- Web/obj/Debug/net5.0/RazorDeclaration/Pages/MainPart/ActiveMathes/AtiveMatches.razor.g.cs
+++ Web/obj/Debug/net5.0/RazorDeclaration/Pages/MainPart/ActiveMathes/AtiveMatches.razor.g.cs
@@ -13,91 +13,91 @@ namespace Test.Pages.MainPart.ActiveMathes
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
+#line 1 "D:\Университет\4 семестр\complex-task-apps\Web\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
+#line 2 "D:\Университет\4 семестр\complex-task-apps\Web\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
+#line 3 "D:\Университет\4 семестр\complex-task-apps\Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
+#line 4 "D:\Университет\4 семестр\complex-task-apps\Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
+#line 5 "D:\Университет\4 семестр\complex-task-apps\Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
+#line 6 "D:\Университет\4 семестр\complex-task-apps\Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
+#line 7 "D:\Университет\4 семестр\complex-task-apps\Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
+#line 8 "D:\Университет\4 семестр\complex-task-apps\Web\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
+#line 9 "D:\Университет\4 семестр\complex-task-apps\Web\_Imports.razor"
 using Test;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\_Imports.razor"
+#line 10 "D:\Университет\4 семестр\complex-task-apps\Web\_Imports.razor"
 using Test.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\Pages\MainPart\ActiveMathes\AtiveMatches.razor"
+#line 2 "D:\Университет\4 семестр\complex-task-apps\Web\Pages\MainPart\ActiveMathes\AtiveMatches.razor"
 using Data.Modal;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\Pages\MainPart\ActiveMathes\AtiveMatches.razor"
+#line 3 "D:\Университет\4 семестр\complex-task-apps\Web\Pages\MainPart\ActiveMathes\AtiveMatches.razor"
 using Data;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\Pages\MainPart\ActiveMathes\AtiveMatches.razor"
+#line 4 "D:\Университет\4 семестр\complex-task-apps\Web\Pages\MainPart\ActiveMathes\AtiveMatches.razor"
 using Microsoft.EntityFrameworkCore;
 
 #line default
@@ -111,7 +111,7 @@ using Microsoft.EntityFrameworkCore;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 75 "D:\Университет\4 семестр\Веб-программирование\complex-task-apps\Web\Pages\MainPart\ActiveMathes\AtiveMatches.razor"
+#line 75 "D:\Университет\4 семестр\complex-task-apps\Web\Pages\MainPart\ActiveMathes\AtiveMatches.razor"
        
     private List<Event> Events { get; set; } = null;
     private Event SelectedMatch { get; set; }
