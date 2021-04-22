591eea97631ed30523d9a86bcb80f64756297697
Максим Худяев <mr.yayay01@bk.ru>
2021-04-22 20:27:37 +0300

    Minor

--- Desktop/Desktop/Desktop/MainWindow.xaml.cs
+++ Desktop/Desktop/Desktop/MainWindow.xaml.cs
@@ -12,31 +12,90 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DbHendler;
+using Npgsql;
+using Test.Data.Modal;
 
 namespace Desktop
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
-        {
-            InitializeComponent();
+	/// <summary>
+	/// Логика взаимодействия для MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
 
-            listSelectingAnAction.SelectedIndex = 0;
-        }
+			listSelectingAnAction.SelectedIndex = 0;
+		}
 
-        private void MenuCloseEvent(object sender, RoutedEventArgs e)
-        {
-            ButtonMenuClose.Visibility = Visibility.Collapsed;
-            ButtonMenuOpen.Visibility = Visibility.Visible;
-        }
-        private void MenuOpenEvent(object sender, RoutedEventArgs e)
-        {
-            ButtonMenuOpen.Visibility = Visibility.Collapsed;
-            ButtonMenuClose.Visibility = Visibility.Visible;
-        }
+		private void MenuCloseEvent(object sender, RoutedEventArgs e)
+		{
+			ButtonMenuClose.Visibility = Visibility.Collapsed;
+			ButtonMenuOpen.Visibility = Visibility.Visible;
+		}
+		private void MenuOpenEvent(object sender, RoutedEventArgs e)
+		{
+			ButtonMenuOpen.Visibility = Visibility.Collapsed;
+			ButtonMenuClose.Visibility = Visibility.Visible;
+		}
 
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			var date = Date.SelectedDate;
+			var team1 = (Teams)(Team1.SelectedItem);
+			var team2 = (Teams)(Team2.SelectedItem);
+
+			DB.Events.Add(new Events() { Id_Sport = 1, Id_Team1 = team1.Id, Id_Team2 = team2.Id, Start_Date = date });
+		}
+
+		DbContext DB { get; set; }
+
+		private void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			String connectionString = "Host=localhost;Port=5432;Database=bookmaker_office;Username=postgres;Password=1234";
+			NpgsqlConnection npgSqlConnection = new NpgsqlConnection(connectionString);
+
+			Grid = editAndAddMatches;
+			Grid.Visibility = Visibility.Visible;
+
+			try
+			{
+				DB = new DbContext(npgSqlConnection);
+
+				foreach (var item in DB.Teams)
+				{
+					Team1.Items.Add(item);
+					Team2.Items.Add(item);
+				}
+			}
+			catch (NpgsqlException ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+		Grid Grid { get; set; }
+
+        private void ListViewItem_Selected(object sender, RoutedEventArgs e)
+        {
+			if (Grid is null)
+				return;
+
+			Grid.Visibility = Visibility.Hidden;
+			Grid = editAndAddMatches;
+			Grid.Visibility = Visibility.Visible;
+		}
+
+        private void ListViewItem_Selected_1(object sender, RoutedEventArgs e)
+		{
+			if (Grid is null)
+				return;
+
+			Grid.Visibility = Visibility.Hidden;
+			Grid = editAndAddBets;
+			Grid.Visibility = Visibility.Visible;
+		}
     }
 }
---
+++ Desktop/Desktop/Desktop/Team.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Test.Data.Modal
+{
+    public partial class Teams
+    {
+        public Teams() { }
+
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int? Raiting { get; set; }
+        public float? percent_win { get; set; }
+        public string Squadlist { get; set; }
+        public string Logo { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Title}";
+        }
+    }
+}
