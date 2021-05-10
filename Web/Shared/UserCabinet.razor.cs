1f859ffa3ceeb20e5cd4993dc841e8f85743812b
Gregory <foxxsf@gmail.com>
2021-05-10 19:09:14 +0300

    minor

---
+++ Web/Shared/UserCabinet.razor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Test.Data.ModalEntity;
+using Microsoft.AspNetCore.Components;
+
+namespace Test.Shared
+{
+    public partial class UserCabinet
+    {
+        [CascadingParameter]
+        public User User { get; set; }
+
+    }
+}
