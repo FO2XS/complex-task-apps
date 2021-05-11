7399436712aed2c26fc1170a6b547e157261e06d
Gregory <foxxsf@gmail.com>
2021-05-12 01:02:23 +0300

    minor

---
+++ Web/Pages/PopUpWindows/FinishBetModal.razor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Test.Data.ModalEntity;
+
+namespace Test.Pages.PopUpWindows
+{
+    public partial class FinishBetModal
+    {
+        [CascadingParameter]
+        private MudDialogInstance MudDialog { get; set; }
+        [Parameter]
+        public Team SelectedTeam { get; set; }
+        [Parameter]
+        public PossibleBet SelectedBet { get; set; }
+        [Parameter]
+        public float SelectedCoef { get; set; }
+
+        private decimal? _sum;
+
+
+        private decimal? CalculatePrize(decimal? sum, float coef)
+        {
+            return sum * Convert.ToDecimal(coef);
+        }
+
+
+        private void MakeBet()
+        {
+
+        }
+    }
+}
---
+++ Web/Pages/PopUpWindows/SelectBetModal.razor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Test.Data.ModalEntity;
+
+namespace Test.Pages.PopUpWindows
+{
+    public partial class SelectBetModal
+    {
+        [CascadingParameter]
+        private MudDialogInstance MudDialog { get; set; }
+        [Parameter]
+        public Event CurrentEvent { get; set; }
+        private List<PossibleBet> Bets { get; set; }
+
+        protected override void OnParametersSet()
+        {
+            if (CurrentEvent != null)
+            {
+                Bets = _service.GetPossibleBets(CurrentEvent);
+            }
+        }
+
+        private void OpenFinishBetModal(Team team, PossibleBet bet)
+        {
+            var coef = CurrentEvent.IdTeam1Navigation == team ? bet.Coef1 : bet.Coef2;
+            var parameters = new DialogParameters
+            {
+                {"SelectedTeam", team}, {"SelectedBet", bet}, {"SelectedCoef", coef}
+            };
+            var result = _dialogService.Show<FinishBetModal>("Заключение ставки", parameters);
+        }
+
+
+        
+    }
+}
