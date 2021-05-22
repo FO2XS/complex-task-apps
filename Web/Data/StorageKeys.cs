af703448d9a46da6082e232fe1e0f41c2a218656
Gregory <foxxsf@gmail.com>
2021-05-22 23:43:25 +0300

    minor

--- Web/Data/StorageKeys.cs
+++ Web/Data/StorageKeys.cs
@@ -5,5 +5,6 @@
         public const string LocalKey = "localInfo";
         public const string SessionKey = "sessionInfo";
         public const string DefaultAvatar = "https://i.imgur.com/aYd3pRc.png";
+        public const int GeneralBetId = 1;
     }
 }
